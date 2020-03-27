05ec3c5dade983c284a0df87e06bf39b9e85e251
emmanuel-carreira <emmanuel_carreira@hotmail.com>
2020-03-27 20:22:56 -0300

    Adds controllers

---
+++ backend/src/controllers/SessionController.js
@@ -0,0 +1,18 @@
+const connection = require('../database/connection');
+
+module.exports = {
+  async create(request, response) {
+    const { id } = request.body;
+
+    const ongName = await connection('ongs')
+      .where('id', id)
+      .select('name')
+      .first();
+
+    if (!ongName) {
+      return response.status(400).json({ error: 'No ONG found with this ID.' });
+    }
+
+    return response.json(ongName);
+  }
+}
